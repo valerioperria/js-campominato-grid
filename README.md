2c1ab2feaf9976b498486c38de37ed6971decb0d
valerioperria <valerio.perria@gmail.com>
2023-10-29 16:43:13 +0100

    complete exercise feat: html - whole content; css -  details; js - eventListner to activate the cell, loop for to color the cell and consol log of cell's content

---
+++ README.md
@@ -0,0 +1,17 @@
+### ESERCIZIO
+
+**Consegna**
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+**Logica programma**
+1. Creare una funzione che:
+- Generi progressivamente dei numeri da un num. min. a un num. max.;
+- Inserisca ogni numero all'interno di un div, con una x classe all'interno.
+2. Inserire i div creati all'interno del DOM html;
+3. Creare un bottone che avvii la griglia;
+4. Creare un ciclo for che:
+- Selezioni una cella al momento del click, cmabiandone il colore;
+- Stampi in console il contenuto della cella selezionata.
--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
-// creare una funzione che generi progressivamente dei numeri da x a y, e che inserisca i numeri all'interno di un div con una classe
+// creare una funzione che generi progressivamente dei numeri da min a max, e che inserisca i numeri all'interno di un div insieme ad una classe (addClass)
 function progressiveNumbers(min, max, addClass) {
     let x = "";
     for (i = min; i <= max; i++) {
         x += `<div class=${addClass}>${i}</div>`;
     }
-    console.log(x)
     return x;
 }
 
 document.querySelector(".grid").innerHTML = progressiveNumbers(1, 100, "cell");
+
+// fare un eventListner che al click sul bottone start avvii la griglia
+
+const btn = document.querySelector("button");
+const grid = document.querySelector(".grid");
+
+btn.addEventListener("click", function () {
+    grid.classList.add("active");
+})
+
+// fare in modo che al click su x cella questa si colori
+
+const cll = document.querySelectorAll(".cell");
+
+for (let i = 0; i < cll.length; i++) {
+    cll[i].addEventListener("click", function () {
+        cll[i].classList.add("cll-active");
+        console.log(parseInt(cll[i].textContent))
+    });
+
+}
+
 
